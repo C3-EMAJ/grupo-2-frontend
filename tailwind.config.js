dd4d1f8bf45767d30eaf96d2b8c8de97c0abf1fc
Samuel Lemos <samuelt.lemos@hotmail.com>
2023-10-02 03:28:17 -0300

    feat: adicionando a paleta de cores do projeto e estilizando o menu lateral

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,19 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      'orange_agenda':'#FF6633',
+      'sidebar_orange':'#FDAE17',
+      'sidebar_grey':'#CDCDCD',
+      'header_grey':'#EEF1F4',
+      'lista_usuarios_grey':'#F8F8F8',
+      'white':'#FFFFFF',
+      'blue_search':'#1890FF',
+      'blue_enviar_arquivo':'#0F91D2',
+      'alert_red':'#FD1717',
+      'green_agendas':'#12BDB2',
+      'black':'#000000'
+    },
     extend: {},
   },
   plugins: [],
