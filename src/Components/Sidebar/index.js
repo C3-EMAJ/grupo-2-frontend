a550a1bd3f24175c96bae7df644a1d9aa2ba9988
Samuel Lemos <samuelt.lemos@hotmail.com>
2023-10-05 20:23:05 -0300

    fix: melhorando a estilização da sidebar

--- src/Components/Sidebar/index.js
+++ src/Components/Sidebar/index.js
@@ -19,54 +19,56 @@ function Sidebar() {
                     </div>
                     <div class="my-10">
                         <div class="flex justify-center items-center">
-                            <img src={foto_perfil} alt="foto do perfil"/>
+                            <img src={foto_perfil} class="rounded-full" alt="foto do perfil"/>
                         </div>
+                        <div class="my-3">
                         <div class="mx-auto text-center text-black font-semibold text-sm">
                             <a>Nome do Usuário</a>
                         </div>
                         <div class="mx-auto text-center text-orange font-medium text-sm">
                             <a>Aluno</a>
                         </div>
+                        </div>
                     </div>
-                    <ul class="space-y-1">
+                    <ul class="space-y-2 mx-3">
                         <li>
-                            <a href="#" class="flex items-center p-1 text-black rounded-lg hover:bg-orange group">
+                            <a href="#" class="flex items-center p-1 text-sm text-black rounded-lg hover:bg-orange group">
                             <img src={img_Processos}></img>
                             <span class="ml-2 whitespace-nowrap">Processos</span>
                             </a>
                         </li>
                         <li>
-                            <a href="#" class="flex items-center p-1 text-black rounded-lg hover:bg-orange group">
+                            <a href="#" class="flex items-center p-1 text-sm text-black rounded-lg hover:bg-orange group">
                             <img src={img_AdicionarUsuario} />
                             <span class="ml-2 whitespace-nowrap">Adicionar Usuário</span>
                             </a>
                         </li>
                         <li>
-                            <a href="#" class="flex items-center p-1 text-black rounded-lg hover:bg-orange group">
+                            <a href="#" class="flex items-center p-1 text-sm text-black rounded-lg hover:bg-orange group">
                             <img src={img_Assistidos} />
                             <span class="ml-2 whitespace-nowrap">Assistidos</span>
                             </a>
                         </li>
                         <li>
-                            <a href="#" class="flex items-center p-1 text-black rounded-lg hover:bg-orange group">
+                            <a href="#" class="flex items-center p-1 text-sm text-black rounded-lg hover:bg-orange group">
                             <img src={img_Ageamentos} />
                             <span class="ml-2 whitespace-nowrap">Agendamentos</span>
                             </a>
                         </li>
                         <li>
-                            <a href="#" class="flex items-center p-1 text-black rounded-lg hover:bg-orange group">
+                            <a href="#" class="flex items-center p-1 text-sm text-black rounded-lg hover:bg-orange group">
                             <img src={img_Pastas} />
                             <span class="ml-2 whitespace-nowrap">Pastas</span>
                             </a>
                         </li>
                         <li>
-                            <a href="#" class="flex items-center p-1 text-black rounded-lg hover:bg-orange group">
+                            <a href="#" class="flex items-center p-1 text-sm text-black rounded-lg hover:bg-orange group">
                             <img src={img_Configuracoes} />
                             <span class="ml-2 whitespace-nowrap">Configurações</span>
                             </a>
                         </li>
                         <li>
-                            <a href="#" class="flex items-center p-1 text-black rounded-lg hover:bg-orange group">
+                            <a href="#" class="flex items-center p-1 text-sm text-black rounded-lg hover:bg-orange group">
                             <img src={img_Sair} />
                             <span class="ml-2 whitespace-nowrap">Sair</span>
                             </a>
