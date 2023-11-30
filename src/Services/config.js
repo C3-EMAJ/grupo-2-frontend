e2e08ba3bca0b26d8b2e2618ee6327690451ff86
Samuel Lemos <samuelt.lemos@hotmail.com>
2023-11-30 17:07:45 -0300

    fix: corrigindo problemas de comunicação com a api

--- src/Services/config.js
+++ src/Services/config.js
@@ -4,7 +4,7 @@ const Api  = axios.create({
     baseURL: process.env.REACT_APP_API,
     headers:{
         "Content-Type":"application/json",
-        Authorization: localStorage.getItem("token")
+        //Authorization: localStorage.getItem("token")
     },
 });
 
